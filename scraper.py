17225525ec2cdd224abedc2c57db39f1a48a866d
Jason Chan <jasonchanhku@gmail.com>
2018-03-21 21:03:12 +0800

    version 1.1 working example

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://www.glassdoor.com/index.htm')
 
 
-driver.find_element_by_css_selector('#KeywordSearch').send_keys('Data Scientist')
+driver.find_element_by_css_selector('#KeywordSearch').send_keys('Art Director')
 
 driver.find_element_by_css_selector('#LocationSearch').clear()
 driver.find_element_by_css_selector('#LocationSearch').send_keys('Hong Kong')
@@ -17,8 +17,9 @@ num_pages = driver.find_elements_by_css_selector('.page a')
 # print(len(links))
 # driver.get(links[0].get_attribute('href'))
 
-for num in range(len(num_pages)):
+end = True
 
+while end:
     links = driver.find_elements_by_css_selector('#MainCol .flexbox .jobLink')
 
     for link in links:
@@ -33,8 +34,13 @@ for num in range(len(num_pages)):
 
     # To prevent selenium returning stalemate element
     # https://stackoverflow.com/questions/45002008/selenium-stale-element-reference-element-is-not-attached-to-the-page
-    next = driver.find_element_by_css_selector('.next a')
-    next.click()
+    try:
+        driver.find_element_by_css_selector('.next a').click()
+    except:
+        end = False
+        break
+
+print('Data successfully scraped')
 
 time.sleep(5)
 driver.close()
